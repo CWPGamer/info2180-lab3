944c12414afcb404591355fad6e73794d3561b3e
Chad <wilsonchad765@gmail.com>
2025-10-17 22:16:56 -0500

    Added hover style on squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,24 +10,26 @@ document.addEventListener("DOMContentLoaded", () => {
     for (const item of squares){
         item.classList.add("square");
         item.addEventListener("click", () =>{
-            // item.classList.add("X");
-            set_square(item);
+            if (turn % 2 == 0){
+                item.classList.remove('O');
+                item.classList.add('X');
+                item.innerHTML = 'X';
+                game_state.push('X');
+            }
+            else{
+                item.classList.remove('X');
+                item.classList.add('O');
+                item.innerHTML = 'O';
+                game_state.push('O');
+            }
+            turn++;
             console.log(game_state);
         });
-    }
-
-    var set_square = function(item) {
-        if (turn % 2 == 0){
-            item.innerHTML = 'X';
-            item.classList.add('X');
-            game_state.push('X');
-        }
-        else{
-            item.innerHTML = 'O';
-            item.classList.add('O');
-            game_state.push('O');         
-        }
-        turn++;
+        item.addEventListener("mouseover", () =>{
+            item.classList.add('hover');
+        })
+        item.addEventListener("mouseout", () =>{
+            item.classList.remove('hover');
+        })
     }
 });
-
