3e1e2627f356975559b840660eeb1e9586e00ca0
Chad <wilsonchad765@gmail.com>
2025-10-17 21:55:15 -0500

    Changed square to 'X' or 'O' when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,33 @@
 "use strict";
 
-window.onload = function(){
+document.addEventListener("DOMContentLoaded", () => {
     // Display the squares on the board
-    var board = document.getElementById("board");
-    console.log(board.children);
-    for (var i = 0; i < board.children.length; i++){
-        board.children[i].className = "square";
+    const board = document.getElementById("board");
+    var squares = board.children;
+    var turn = 0;
+    var game_state = [];
+
+    for (const item of squares){
+        item.classList.add("square");
+        item.addEventListener("click", () =>{
+            // item.classList.add("X");
+            set_square(item);
+            console.log(game_state);
+        });
     }
-}
+
+    var set_square = function(item) {
+        if (turn % 2 == 0){
+            item.innerHTML = 'X';
+            item.classList.add('X');
+            game_state.push('X');
+        }
+        else{
+            item.innerHTML = 'O';
+            item.classList.add('O');
+            game_state.push('O');         
+        }
+        turn++;
+    }
+});
+
